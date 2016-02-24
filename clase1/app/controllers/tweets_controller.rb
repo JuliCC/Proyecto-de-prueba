0728d49e28e95893671224923c9b819a508df4ef
Julieth Cardona <juli-c.c@hotmail.es>
2016-02-24 17:08:39 -0500

    Edit

--- clase1/app/controllers/tweets_controller.rb
+++ clase1/app/controllers/tweets_controller.rb
@@ -1,19 +1,30 @@
 class TweetsController < ApplicationController
-	def index
-		@tweets = Tweet.all 
-	end
-		
-	def new
-		@tweet = Tweet.new
-	end
+    def index
+        @tweets = Tweet.all 
+    end
+        
+    def new
+        @tweet = Tweet.new
+    end
 
-	def create
-		@tweet = Tweet.new(tweet_params)
-		@tweet.save
-		redirect_to tweets_path
-	end
+    def edit
+        @tweet = Tweet.find(params[:id])
+    end
 
-	def tweet_params
-		params.require(:tweet).permit(:message)
-	end
+    def create
+        @tweet = Tweet.new(tweet_params)
+        @tweet.save
+        redirect_to tweets_path
+    end
+
+    def update
+        @tweet = Tweet.find(params[:id])
+        @tweet.update(tweet_params)
+        redirect_to tweets_path
+    end
+
+
+    def tweet_params
+        params.require(:tweet).permit(:id,:message)
+    end
 end
